0f8dff17d62d7f58d240bef62ec7129b352d14bd
Teo Grozdanoski <teo.grozdanoski@gmail.com>
2023-01-09 16:55:57 +0100

    Initial commit

---
+++ src/main/java/mk/kalitek/javaweb2telegram/JavaWeb2telegramApplication.java
@@ -0,0 +1,13 @@
+package mk.kalitek.javaweb2telegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWeb2telegramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWeb2telegramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mk/kalitek/javaweb2telegram/JavaWeb2telegramApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.kalitek.javaweb2telegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWeb2telegramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
